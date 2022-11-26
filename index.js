f0ce9964de8235812f3a5acae9ccce394df98586
RST786coder <akshay786choyal@gmail.com>
2022-11-26 15:44:43 +0530

    added cors

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const home = require("./routes/home");
 const app = express();
 const port = process.env.PORT || 3000;
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
